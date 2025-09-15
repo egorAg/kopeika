5e29a9f278b075ba567cfe22fe56a9eaf49c8f3c
eageev <eageev@kozhin.dev>
2025-09-15 16:51:19 +0300

    фиксы миграций и обновление ридми

--- api/src/migrations/1757857442310-Init.ts
+++ api/src/migrations/1757857442310-Init.ts
@@ -1,16 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1757857442310 implements MigrationInterface {
-    name = 'Init1757857442310'
+  name = 'Init1757857442310';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "auth_sessions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "refreshTokenHash" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "ip" character varying, "userAgent" character varying, "expiresAt" TIMESTAMP WITH TIME ZONE, "lastUsedAt" TIMESTAMP WITH TIME ZONE, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "PK_641507381f32580e8479efc36cd" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "auth_sessions" ADD CONSTRAINT "FK_925b24d7fc2f9324ce972aee025" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "auth_sessions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "refreshTokenHash" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "ip" character varying, "userAgent" character varying, "expiresAt" TIMESTAMP WITH TIME ZONE, "lastUsedAt" TIMESTAMP WITH TIME ZONE, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "PK_641507381f32580e8479efc36cd" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auth_sessions" ADD CONSTRAINT "FK_925b24d7fc2f9324ce972aee025" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "auth_sessions" DROP CONSTRAINT "FK_925b24d7fc2f9324ce972aee025"`);
-        await queryRunner.query(`DROP TABLE "auth_sessions"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "auth_sessions" DROP CONSTRAINT "FK_925b24d7fc2f9324ce972aee025"`,
+    );
+    await queryRunner.query(`DROP TABLE "auth_sessions"`);
+  }
 }
--- api/src/migrations/1757935756587-Init.ts
+++ api/src/migrations/1757935756587-Init.ts
@@ -1,24 +1,41 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1757935756587 implements MigrationInterface {
-    name = 'Init1757935756587'
+  name = 'Init1757935756587';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "guild_balance_history" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "date" date NOT NULL, "balance" numeric NOT NULL, "guildId" uuid, CONSTRAINT "PK_b5521e12374bf831f38ca602841" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "guilds" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "dailyMinAmount" integer NOT NULL DEFAULT '1000', "balance" numeric NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e7e7f2a51bd6d96a9ac2aa560f9" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "guild_users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "guildId" uuid, "userId" uuid, CONSTRAINT "PK_b7c193a6ff9298b40b684af968f" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "guild_balance_history" ADD CONSTRAINT "FK_a02605ad60c2fae86aa51b633ec" FOREIGN KEY ("guildId") REFERENCES "guilds"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "guild_users" ADD CONSTRAINT "FK_7d09c2cd15867f8482a88b70c55" FOREIGN KEY ("guildId") REFERENCES "guilds"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "guild_users" ADD CONSTRAINT "FK_3fabcb4a767c5d9cb607cd5ea24" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "guild_balance_history" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "date" date NOT NULL, "balance" numeric NOT NULL, "guildId" uuid, CONSTRAINT "PK_b5521e12374bf831f38ca602841" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "guilds" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "dailyMinAmount" integer NOT NULL DEFAULT '1000', "balance" numeric NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e7e7f2a51bd6d96a9ac2aa560f9" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "guild_users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "guildId" uuid, "userId" uuid, CONSTRAINT "PK_b7c193a6ff9298b40b684af968f" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "guild_balance_history" ADD CONSTRAINT "FK_a02605ad60c2fae86aa51b633ec" FOREIGN KEY ("guildId") REFERENCES "guilds"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "guild_users" ADD CONSTRAINT "FK_7d09c2cd15867f8482a88b70c55" FOREIGN KEY ("guildId") REFERENCES "guilds"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "guild_users" ADD CONSTRAINT "FK_3fabcb4a767c5d9cb607cd5ea24" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "guild_users" DROP CONSTRAINT "FK_3fabcb4a767c5d9cb607cd5ea24"`);
-        await queryRunner.query(`ALTER TABLE "guild_users" DROP CONSTRAINT "FK_7d09c2cd15867f8482a88b70c55"`);
-        await queryRunner.query(`ALTER TABLE "guild_balance_history" DROP CONSTRAINT "FK_a02605ad60c2fae86aa51b633ec"`);
-        await queryRunner.query(`DROP TABLE "guild_users"`);
-        await queryRunner.query(`DROP TABLE "guilds"`);
-        await queryRunner.query(`DROP TABLE "guild_balance_history"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "guild_users" DROP CONSTRAINT "FK_3fabcb4a767c5d9cb607cd5ea24"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "guild_users" DROP CONSTRAINT "FK_7d09c2cd15867f8482a88b70c55"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "guild_balance_history" DROP CONSTRAINT "FK_a02605ad60c2fae86aa51b633ec"`,
+    );
+    await queryRunner.query(`DROP TABLE "guild_users"`);
+    await queryRunner.query(`DROP TABLE "guilds"`);
+    await queryRunner.query(`DROP TABLE "guild_balance_history"`);
+  }
 }
